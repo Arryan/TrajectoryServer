446e447f3db85438bb696912ffb70754b8659532
derek <derek@llamabagel.ca>
2019-08-24 22:30:54 -0400

    Finished GO Train APIs

--- trains.js
+++ trains.js
@@ -1,9 +1,14 @@
 const fetch = require('node-fetch')
 const ENDPOINT = 'http://api.openmetrolinx.com/OpenDataAPI/'
 const API_KEY = '30021130'
+const stations = require('./stations').stations
+const geometry = require('./tracks.json')
+
+const turf = require('@turf/turf')
 
 exports.getData = async function() {
     let trains = await getTrainPositions()
+    trains = trains.filter(train => train.LineCode == "LE" && train.VariantDir == "E")
 
     return (await Promise.all(trains.map(async (train) => {
         let next = await getStationArrival(train.NextStopCode, train.TripNumber)
@@ -15,7 +20,7 @@ exports.getData = async function() {
             location: [train.Latitude, train.Longitude],
             nextStation: train.NextStopCode,
             arrivalTime: time,
-            distance: 0
+            distance: getDistanceToStation(train.NextStopCode, [train.Latitude, train.Longitude])
         }
     })))
     .filter(train => train != null)
@@ -45,4 +50,12 @@ async function getStationArrival(station, train) {
 async function getTrainPositions(){
     let response = await fetch(`${ENDPOINT}/api/V1/ServiceataGlance/Trains/All/?key=${API_KEY}`)
     return (await response.json()).Trips.Trip
+}
+
+function getDistanceToStation(stationCode, trainLocation) {
+    let line = turf.lineString(geometry.features[0].geometry.coordinates)
+    let station = stations[stationCode]
+
+    let sliced = turf.lineSlice(turf.point([station[1], station[0]]), turf.point([trainLocation[1], trainLocation[0]]), line)
+    return turf.length(sliced, {units: 'kilometers'})
 }
